8fd5fe9450fef9428b0e631b65ea7897bc426df3
Prabhdit-Singh <54041431+Prabhdit-Singh@users.noreply.github.com>
2021-05-02 23:53:07 +0530

    Taking input from user

---
+++ inputOutput.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int num; // declare an integer variable to store a value
+    // Asking user to enter a number.
+    cout<<"Enter a number: "; 
+    //cin is used to take user input.
+    cin>>num;
+    cout<<"You entered: "<<num;
+    return 0;
+}
