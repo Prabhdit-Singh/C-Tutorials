3fb9619b0b8019d8f1200bbf313f0c3e529ad20a
Prabhdit-Singh <54041431+Prabhdit-Singh@users.noreply.github.com>
2021-05-03 00:08:26 +0530

    Program to display sum and difference.

---
+++ sum and difference.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n1,n2;
+    cout<<"Enter two numbers: ";
+    cin>>n1>>n2;
+    cout<<"The sum of the two numbers is: "<<n1+n2;
+    cout<<"\nThe diffrence of the two numbers is: "<<n1-n2;
+    return 0;
+}
