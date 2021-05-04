b9f401402a940b7d7098c4a637c2e0f18cff9720
Prabhdit-Singh <54041431+Prabhdit-Singh@users.noreply.github.com>
2021-05-05 00:01:49 +0530

    Division file added

---
+++ Division.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n1,n2;
+    cout<<"Enter two numbers: ";
+    cin>>n1>>n2;
+    cout<<"The division of the two numbers is: "<<n1/n2;
+    cout<<"\nThe diffrence of the two numbers is: "<<n1-n2;
+    return 0;
+}
