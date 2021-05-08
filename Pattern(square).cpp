4e7efd7839a5515292fbeb118ed921282055d850
Prabhdit-Singh <54041431+Prabhdit-Singh@users.noreply.github.com>
2021-05-08 23:14:55 +0530

    Program to print square pattern.

---
+++ Pattern(square).cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main(){
+    int n;
+
+    cout<<"Enter sid eof square for pattern: "<<endl;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+        cout<<"X ";
+    }
+            cout<<"\n";
+}
+        return 0;
+    
+}
